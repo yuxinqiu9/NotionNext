48c63b99bfd72265632fb9345d5c5bec982b6581
yuxinqiu <145776053+yuxinqiu9@users.noreply.github.com>
2025-10-12 00:14:12 +0200

    Update config.js

--- themes/example/config.js
+++ themes/example/config.js
@@ -3,10 +3,10 @@
  */
 const CONFIG = {
   // 菜单配置
-  EXAMPLE_MENU_CATEGORY: true, // 显示分类
-  EXAMPLE_MENU_TAG: true, // 显示标签
-  EXAMPLE_MENU_ARCHIVE: true, // 显示归档
-  EXAMPLE_MENU_SEARCH: true, // 显示搜索
+  EXAMPLE_MENU_CATEGORY: false, // 显示分类
+  EXAMPLE_MENU_TAG: false, // 显示标签
+  EXAMPLE_MENU_ARCHIVE: false, // 显示归档
+  EXAMPLE_MENU_SEARCH: false, // 显示搜索
 
   EXAMPLE_POST_LIST_COVER: true, // 列表显示文章封面
 
