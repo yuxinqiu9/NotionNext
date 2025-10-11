ff6754b0b88005a9a6c76032317f51a47ddaade6
yuxinqiu <145776053+yuxinqiu9@users.noreply.github.com>
2025-10-12 01:07:07 +0200

    Update SideBar.js

--- themes/example/components/SideBar.js
+++ themes/example/components/SideBar.js
@@ -43,35 +43,6 @@ export const SideBar = props => {
         </aside>
       )}
 
-      {/* 分类 */}
-      <aside className='w-full rounded shadow overflow-hidden mb-6'>
-        <h3 className='text-sm bg-gray-100 text-gray-700 dark:bg-hexo-black-gray dark:text-gray-200 py-3 px-4 dark:border-hexo-black-gray border-b'>
-          {locale.COMMON.CATEGORY}
-        </h3>
-
-        <div className='p-4'>
-          <ul className='list-reset leading-normal'>
-            {categoryOptions?.map(category => {
-              return (
-                <SmartLink
-                  key={category.name}
-                  href={`/category/${category.name}`}
-                  passHref
-                  legacyBehavior>
-                  <li>
-                    {' '}
-                    <a
-                      href={`/category/${category.name}`}
-                      className='text-gray-darkest text-sm hover:underline'>
-                      {category.name}({category.count})
-                    </a>
-                  </li>
-                </SmartLink>
-              )
-            })}
-          </ul>
-        </div>
-      </aside>
 
       {/* 最新文章 */}
       <aside className='w-full rounded shadow overflow-hidden mb-6'>
