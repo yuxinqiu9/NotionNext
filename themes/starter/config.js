44eed5ae5fa9f1750f9bc9b5d81c62affd3b72e7
yuxinqiu <145776053+yuxinqiu9@users.noreply.github.com>
2025-10-11 23:13:37 +0200

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -206,27 +206,17 @@ const CONFIG = {
 
   // 这里不支持CONFIG和环境变量，需要一一修改此处代码。
   STARTER_TEAM_ITEMS: [
-    {
-      STARTER_TEAM_ITEM_AVATAR:
-        'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fa06c61bb-980e-4180-bc18-c15f92c78bb4%2Ftangly1024.jpg?table=collection&id=8e7acf17-de09-4fa1-abde-b5b80ad4a813&t=8e7acf17-de09-4fa1-abde-b5b80ad4a813&width=100&cache=v2',
-      STARTER_TEAM_ITEM_NICKNAME: 'Tangly',
-      STARTER_TEAM_ITEM_DESCRIPTION: 'Developer'
-    },
-    {
-      STARTER_TEAM_ITEM_AVATAR: '/images/starter/team/team-01.png',
-      STARTER_TEAM_ITEM_NICKNAME: 'Melissa Tatcher',
-      STARTER_TEAM_ITEM_DESCRIPTION: 'Marketing Expert'
-    },
-    {
-      STARTER_TEAM_ITEM_AVATAR: '/images/starter/team/team-02.png',
-      STARTER_TEAM_ITEM_NICKNAME: 'Stuard Ferrel',
-      STARTER_TEAM_ITEM_DESCRIPTION: 'Digital Marketer'
-    },
-    {
-      STARTER_TEAM_ITEM_AVATAR: '/images/starter/team/team-03.png',
-      STARTER_TEAM_ITEM_NICKNAME: 'Eva Hudson',
-      STARTER_TEAM_ITEM_DESCRIPTION: 'Creative Designer'
-    }
+     {
+        STARTER_TEAM_ITEM_AVATAR:
+          '/images/starter/team/team-00.png',
+        STARTER_TEAM_ITEM_NICKNAME: 'Yuxin Qiu',
+        STARTER_TEAM_ITEM_DESCRIPTION: 'Developer'
+      },
+      {
+        STARTER_TEAM_ITEM_AVATAR: '/images/starter/team/team-01.png',
+        STARTER_TEAM_ITEM_NICKNAME: 'Shuangying Xu',
+        STARTER_TEAM_ITEM_DESCRIPTION: 'Developer'
+      }
   ],
 
   // 博客文章区块
